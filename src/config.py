cb659b5473c7b5341eb5f863408cd51cc3457a12
TheMedicineSeller <nishanth21902@gmail.com>
2023-08-31 07:32:11 +0530

    changed paths, batch size, epochs, device and removed worker threads for torch.Dataset object

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
-DATA_DIR = '/Users/samik/Desktop/Programming/CaptchaCrypt/captcha_images_v2/'
-BATCH_SIZE = 8  #try and change to 16 later and see  
+DATA_DIR = '../data/captcha_images_v2/'
+MODEL_PATH = '../model_saves/model.bin'
+BATCH_SIZE = 32  #try and change to 16 later and see  
 IMAGE_WIDTH = 300
 IMAGE_HEIGHT = 75
-NUM_WORKERS = 8 #depends on the device
-EPOCHS = 100 #keeping it low for initial training -> will increase later
-DEVICE = "cpu" #device for macOS -> cpu not cuda sad
-
+# NUM_WORKERS = 2 #depends on the device
+EPOCHS = 200 #keeping it low for initial training -> will increase later
+DEVICE = "cuda:0"
