7a01354e9cf10180fcd620a2f69e13956dfe5b42
TheMedicineSeller <nishanth21902@gmail.com>
2023-10-11 11:07:07 +0530

    Changed training params (just change model path to your path)

--- src/config.py
+++ src/config.py
@@ -1,8 +1,7 @@
-DATA_DIR = '../data/captcha_images_v2/'
-MODEL_PATH = '../model_saves/model.bin'
-BATCH_SIZE = 32  #try and change to 16 later and see  
-IMAGE_WIDTH = 300
-IMAGE_HEIGHT = 75
-# NUM_WORKERS = 2 #depends on the device
+DATA_DIR = '../data/vtop_captchas/'
+MODEL_PATH = '../model_saves/vtop_model.bin'
+BATCH_SIZE = 64  #try and change to 16 later and see  
+IMAGE_WIDTH = 260
+IMAGE_HEIGHT = 50
 EPOCHS = 200 #keeping it low for initial training -> will increase later
 DEVICE = "cuda:0"
