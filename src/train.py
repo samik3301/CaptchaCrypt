f852f42dd9763885de7e3b5ec0fd4084c91c19d9
TheMedicineSeller <nishanth21902@gmail.com>
2023-11-10 15:15:34 +0530

    How did i miss the train file ...

--- src/train.py
+++ src/train.py
@@ -1,117 +1,116 @@
-import os
-import glob
 import torch
+import glob
 import numpy as np
-
+from torch.utils.data import DataLoader
 from sklearn import preprocessing
 from sklearn import model_selection
-from sklearn import metrics
-
-import config
-import dataset
-import engine
-
-from model import CaptchaModel
+from matplotlib import pyplot as plt
 from pprint import pprint
 
+import _config
+from _model import CaptchaLoss, CaptchaModel
+from _engine import train_epoch, evaluate
+from _dataset import DataWrapper
 
-def decode_predictions(preds,encoder):
-    preds = preds.permute(1,0,2)  #bs, timestamps, predictions
-    preds = torch.softmax(preds,2)
-    preds = torch.argmax(preds,2)
-    preds = preds.detach().cpu().numpy()
-    cap_preds = []
-    for j in range(preds.shape[0]):
-        temp = []
-        for k in preds[j,:]:
-            k = k-1
-            if k == -1:
-                temp.append("-")
+def decode_predictions(preds, encoder):
+    # pred - (bs X timesteps X vocabsize) -> (bs X time_steps) -> (bs X 6)
+    bs = preds.size(0)
+    preds = torch.softmax(preds, 2)
+    preds = torch.argmax(preds, 2)
+    preds = preds.detach().cpu().numpy() # (bs x timesteps)
+    decoded_batch = []
+    for i in range(bs):
+        decoded = []
+        for token in preds[i, :]:
+            if token == 0:
+                decoded.append('-')
             else:
-                temp.append(encoder.inverse_transform([k])[0])
-        tp = "".join(temp)
-        cap_preds.append(tp)
-    return cap_preds
+                decoded.append(encoder.inverse_transform([token - 1])[0])
+        decoded_str = "".join(decoded)
+        decoded_batch.append(decoded_str)
+    return decoded_batch
 
+def plot_lossgraph(train_values, val_values, n_epochs, modelname):
+    eps = np.arange(1, n_epochs+1)
+    plt.plot(eps, train_values, label='Training Loss')
+    plt.plot(eps, val_values, label='Validation Loss')
+    
+    # Add in a title and axes labels
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    
+    # Set the tick locations
+    plt.xticks(np.arange(0, n_epochs+1, 25))
+    
+    # Display the plot
+    plt.legend(loc='best')
+    
+    plt.savefig(_config.PLOTS_SAVE_PATH + modelname + "_loss.jpg")
 
+def train(model, train_loader, val_loader, loss_func, optimizer, scheduler, label_encoder, actual_captchas):
 
-def run_training():
-    # image_files = glob.glob(config.DATA_DIR + "*.png")  #scanning through all the files within a folder
-    image_files = glob.glob("D:/Python/CaptchaCrypt/data/vtop_captchas/*")  #scanning through all the files within a folder
-    #/../..abcde.png -> [['a','b','c','d','e']]
+    dev = torch.device(_config.DEVICE if torch.cuda.is_available() else "cpu")
+    train_losses = []
+    val_losses = []
+    for epoch in range(_config.EPOCHS):
+        train_loss = train_epoch(model, train_loader, loss_func, optimizer, dev)
+        val_preds, val_loss  = evaluate(model, val_loader, loss_func, dev)
+        scheduler.step(val_loss)
+
+        captcha_predicted  = []
+        for pred in val_preds:
+            pred_decoded = decode_predictions(pred, label_encoder)
+            captcha_predicted.extend(pred_decoded)
+        pprint(list(zip(actual_captchas, captcha_predicted))[6:11])
+        print(f"Epoch: {epoch}, train_loss={train_loss}, valid_loss={val_loss}")
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+    
+    plot_lossgraph(np.array(train_losses), np.array(val_losses), n_epochs=_config.EPOCHS, modelname="vtopnew")
+
+if __name__ == "__main__":
+    # Loading Labels
+    image_files = glob.glob(_config.DATA_DIR + "*.png")
+    if len(image_files) == 0:
+        print("ERROR reading images at path, Check path ..")
+        exit(0)
+    
     targets_org = [x.split("\\")[-1][:-4] for x in image_files]
-    #abcde - > [a,b,c,d,e]
     targets = [[c for c in x] for x in targets_org]
-    targets_flat = [c for clist in targets for c in clist]  #flattening the list
-    #converting a multi dimensional list to a single dimensional list before encoding it 
 
+    targets_flat = [c for clist in targets for c in clist]
     label_encoder = preprocessing.LabelEncoder()
     label_encoder.fit(targets_flat)
 
     target_encoded = [label_encoder.transform(x) for x in targets]
     target_encoded = np.array(target_encoded)+1
-    #print(target_encoded)
-    #print("\n")
-    #print(len(label_encoder.classes_))  #we have 19 different label encoded classes
-    #print(targets)
-    #print("\n")
-    #print(np.unique(targets_flat))
 
-
-    #NOW SPLITTING THE DATA
-    train_images, val_images, train_targets, val_targets, train_orig_targets, test_orig_targets = model_selection.train_test_split(
+    print(f"Loaded {len(image_files)} images and labels..\nFound {len(label_encoder.classes_)} different characters in vocabulary")
+    # Loading images : we dont need train_original_targets, we just are comparing with the original targets at validation time
+    train_images, val_images, train_targets, val_targets, _, val_original_targets = model_selection.train_test_split(
         image_files,
         target_encoded,
         targets_org,
         test_size=0.1,
         random_state= 42
     )
+    train_dataset = DataWrapper(paths = train_images, labels = train_targets, resize=(_config.IMAGE_HEIGHT,_config.IMAGE_WIDTH))
+    train_loader  = DataLoader(train_dataset, _config.BATCH_SIZE, shuffle=False)
+    val_dataset   = DataWrapper(paths = val_images, labels = val_targets, resize=(_config.IMAGE_HEIGHT,_config.IMAGE_WIDTH))
+    val_loader    = DataLoader(val_dataset, _config.BATCH_SIZE, shuffle=False)
 
-    train_dataset = dataset.Classification(
-        image_paths = train_images, 
-        targets = train_targets, 
-        resize=(config.IMAGE_HEIGHT,config.IMAGE_WIDTH)
-    )
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size = config.BATCH_SIZE,
-        shuffle = True
-    )
-
-    val_dataset = dataset.Classification(
-        image_paths = val_images, 
-        targets = val_targets, 
-        resize=(config.IMAGE_HEIGHT,config.IMAGE_WIDTH)
-    )
-
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size = config.BATCH_SIZE,
-        shuffle = False
-    )
-
-    model = CaptchaModel(num_chars=len(label_encoder.classes_))
-    model.to(torch.device(config.DEVICE))
-
-    optimizer = torch.optim.Adam(model.parameters(), lr = 3e-4)
+    # Model, loss func, opt
+    model = CaptchaModel(vocabulary_size = len(label_encoder.classes_))
+    model.to(torch.device(_config.DEVICE if torch.cuda.is_available() else "cpu"))
+    loss = CaptchaLoss()
+    optimizer = torch.optim.Adam(model.parameters())
     scheduler =  torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer,factor=0.8,patience=5,verbose=True
+        optimizer, factor=0.8, patience=5, verbose=True
     )
-    for epoch in range(config.EPOCHS):
-        train_loss = engine.train_fn(model, train_loader, optimizer)
-        valid_preds,valid_loss  = engine.eval_fn(model, val_loader)
-        valid_cap_preds  = []
-        for vp in valid_preds:
-            current_preds = decode_predictions(vp,label_encoder)
-            valid_cap_preds.extend(current_preds)
-        pprint(list(zip(test_orig_targets, valid_cap_preds))[6:11]) #to test change the list also 
-        print(f"Epoch: {epoch}, train_loss={train_loss}, valid_loss={valid_loss}")  
-
-    torch.save(model.state_dict(), 'D:/Python/CaptchaCrypt/model_saves/vtop_model.bin') # From here one save model.state_dict NIBA not the entire model object
-
-if __name__ == "__main__":
-    run_training()
-    #with every step we are predicting 75 values: 
-    #the unknown label is not 0 , lets say it is ! or the smiley "!11123343544rf!!!!!!EF" and the size of it will be 75
-    #now we have to remove the unknown prediction and count unique occurences
+    # Train and save
+    print("Initiate Training ...\n\n")
+    train(model, train_loader, val_loader, loss, optimizer, scheduler, label_encoder, val_original_targets)
+    torch.save(model.state_dict(), _config.MODEL_PATH)
+    
